61e016ec602bf7c1f846c02ef95d2949aabe3c55
rahul kumar <142343501+CodeSciRahul@users.noreply.github.com>
2024-04-03 01:10:32 +0530

    singup is completed

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -8,14 +8,41 @@ function Signup() {
   const [password, setpassword] = useState('')
   const [name, setname] = useState('')
   const [cnfpassword, setcnfpassword] = useState('')
+  const [button1Disabled, setButton1Disabled] = useState(false);
+  const [button2Disabled, setButton2Disabled] = useState(false);
+
+  const handleClick = (buttonClicked) => {
+    if (buttonClicked === 'button1') {
+      console.log("Role: Mentor")
+      setButton1Disabled(true);
+      setButton2Disabled(false);
+    } else if (buttonClicked === 'button2') {
+      console.log("Role: Mentee")
+      setButton1Disabled(false);
+      setButton2Disabled(true);
+    }
+  };
+
+  
 
   
 
   const handlebtn = (e)=>{
     e.preventDefault()
     const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if(regex.test(password))
+    if (!button1Disabled && !button2Disabled) {
+      toast.error("Please select a role (Mentor or Mentee) to proceed.", {
+        duration: 5000
+      });
+    }
+    else if (!emailRegex.test(email)) {
+      toast.error("Please enter a valid email address", {
+        duration: 5000
+      });
+    }
+    else if(regex.test(password))
     {
       toast.success("successfully create an account",{
         duration: 2000
@@ -24,6 +51,12 @@ function Signup() {
     else if(password!==cnfpassword)
     {
       toast.error("passord not match",{
+        duration:5000
+      })
+    }
+    else if(email==='' || name==='')
+    {
+      toast.error("kindly fill all the detail",{
         duration:5000
       })
     }
@@ -48,6 +81,22 @@ function Signup() {
     <div className='text-center my-12 text-5xl font-semibold'><h1>Create an account</h1></div>
     <form action="" className='rounded-sm min-w-[430px] bg-gray-100 shadow-[var(--shadow-md)]'>
         <div className='pt-12 px-12 '>
+          <div className=' flex justify-center gap-14 h-[45px]'>
+          <button
+        onClick={() => handleClick('button1')}
+        className={`hover:outline-none ${button1Disabled ? 'button-style' : ''}`}
+        disabled={button1Disabled}
+      >
+        Mentor
+      </button>
+      <button
+        onClick={() => handleClick('button2')}
+        className={`hover:outline-none ${button2Disabled ? 'button-style' : ''}`}
+        disabled={button2Disabled}
+      >
+        Mentee
+      </button>
+          </div>
         <div className='flex flex-col  pt-5'>
             <label className='mb-2'>Full name</label>
             <input 
@@ -55,8 +104,8 @@ function Signup() {
             placeholder='enter your full name'
             autoComplete='true'
             className='rounded-sm px-2 py-2 border border-solid border-black'
-            required
             value={name} 
+            required
             onChange={(e)=> setname(e.target.value)}/>
         </div>
         <div className='flex flex-col my-5 '>
