5a8934dac638cb1e87863d3ff0519faa41d89ede
Abhisekh Upadhaya <Abhisek0721@gmail.com>
2024-04-06 22:56:19 +0530

    mentee can view and join sessions

--- src/pages/MenteeSession.jsx
+++ src/pages/MenteeSession.jsx
@@ -1,5 +1,15 @@
+import CreateMeetingModal from "../components/CreateMeeting";
+import SessionTableOfMentor from "../components/SessionTableOfMentor";
+
 const MenteeSession = () => {
-  return <div>for mentee session</div>;
+  return (
+    <div className="heading1">
+      <div>
+        <h1 className="ml-10 text-2xl">All Upcoming and Live Sessions</h1>
+      </div>
+      <SessionTableOfMentor />
+    </div>
+  );
 };
 
 export default MenteeSession;
